f2f46e24ba490ac4bc3b36067a99668f28f451a9
Onur Anil <onuranil749@gmail.com>
2024-05-02 22:09:13 +0300

    Initial Commit

---
+++ FireBaseInstaClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireBaseInstaClone
+//
+//  Created by Onur Anıl on 2.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
